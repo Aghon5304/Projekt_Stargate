16e5ec60156b82cd8aad94c0fe45c623aedb7a8d
Oliwier Matus <matusoliwier@gmail.com>
2025-03-23 21:44:53 +0100

    added basic inventory interaction

--- Scenes/Entities/Test_box/test_box.gd
+++ Scenes/Entities/Test_box/test_box.gd
@@ -1,21 +1,36 @@
 extends Node3D
-@export var ItemId := GlobalItems.ItemTypes.LATARKA
-@onready var gracz: CharacterBody3D = $"../../../Gracz"
 @onready var navigation_obstacle_3d: NavigationObstacle3D = $StaticBody3D/NavigationObstacle3D
 
+@export var ItemId := GlobalItems.ItemTypes.LATARKA
+@export var playerInRange: bool = false 
 
-func _on_static_body_3d_input_event(camera: Node, event: InputEvent, event_position: Vector3, normal: Vector3, shape_idx: int) -> void:
+func _on_static_body_3d_input_event(_camera: Node, event: InputEvent, _event_position: Vector3, _normal: Vector3, _shape_idx: int) -> void:
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed == true:
-			for item in GlobalItems.Ekwipunek.size():
-				if GlobalItems.Ekwipunek[item] == null:
-					GlobalItems.Ekwipunek[item] = ItemId
-					self.visible = false	
-					navigation_obstacle_3d.avoidance_layers = 2
-					var collision_shape_3d: CollisionShape3D = $StaticBody3D/CollisionShape3D
-					collision_shape_3d.disabled
-					var navigation_region_3d: NavigationRegion3D = $"../.."
-					navigation_region_3d.bake_navigation_mesh()
+			GlobalInput.Last_clicked = self
+			while playerInRange == false:
+				if GlobalInput.Last_clicked != self:
 					return
-			print("masz za duzo itemów w eq")
+				await get_tree().process_frame
+			if (playerInRange == true):
+				for item in GlobalItems.Ekwipunek.size():
+					if GlobalItems.Ekwipunek[item] == null:
+						GlobalItems.Ekwipunek[item] = ItemId
+						self.visible = false	
+						var collision_shape_3d: CollisionShape3D = $StaticBody3D/CollisionShape3D
+						collision_shape_3d.disabled = true
+						var navigation_region_3d: NavigationRegion3D = $"../.."
+						return
+				print("masz za duzo itemów w eq")
+	pass # Replace with function body.
+
+
+
+func _on_pick_up_range_area_entered(_area: Area3D) -> void:
+	playerInRange = true
+	pass # Replace with function body.
+
+
+func _on_pick_up_range_area_exited(_area: Area3D) -> void:
+	playerInRange = false
 	pass # Replace with function body.
