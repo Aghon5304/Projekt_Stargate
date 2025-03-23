ad7f6da6dd848a182aa4d088bd955ad8d5c6c819
Oliwier Matus <matusoliwier@gmail.com>
2025-03-23 21:44:53 +0100

    added basic inventory interaction

---
+++ Scenes/Menus/equipment_bar/item_button.gd
@@ -0,0 +1,17 @@
+extends Button
+@export var button_number : int
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_pressed() -> void:
+	print(button_number)
+	print(GlobalItems.Ekwipunek[button_number])
+	pass # Replace with function body.
