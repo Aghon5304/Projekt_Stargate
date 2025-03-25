62269862d92551f12c1d356c80fdf5dc6049ec5c
Oliwier Matus <matusoliwier@gmail.com>
2025-03-26 00:40:26 +0100

    Added simple crafting

--- Scenes/Global/Items/Global_Items.gd
+++ Scenes/Global/Items/Global_Items.gd
@@ -1,7 +1,13 @@
 extends Node
-var Ekwipunek := [null,null,null,null,null,null,null,null]
+var Ekwipunek := [-1,-1,-1,-1,-1,-1,-1,-1]
 enum ItemTypes {
 	LATARKA,
 	PISTOLET,
 	NOTATNIK,
+	PISTOLET_Z_LATARKA
+}
+#[item nr 1 , item nr 2 , wynik]
+var Crafting := {
+	Vector2(ItemTypes.LATARKA,ItemTypes.LATARKA): ItemTypes.PISTOLET,
+	Vector2(ItemTypes.LATARKA,ItemTypes.PISTOLET): ItemTypes.NOTATNIK,
 }
