04f500de29def0de0cf90be0535b40d36ddf498f
Oliwier Matus <matusoliwier@gmail.com>
2025-03-26 00:40:26 +0100

    Added simple crafting

--- Scenes/Entities/Test_box/test_box.gd
+++ Scenes/Entities/Test_box/test_box.gd
@@ -1,5 +1,4 @@
 extends Node3D
-@onready var navigation_obstacle_3d: NavigationObstacle3D = $StaticBody3D/NavigationObstacle3D
 
 @export var ItemId := GlobalItems.ItemTypes.LATARKA
 @export var playerInRange: bool = false 
@@ -14,7 +13,7 @@ func _on_static_body_3d_input_event(_camera: Node, event: InputEvent, _event_pos
 				await get_tree().process_frame
 			if (playerInRange == true):
 				for item in GlobalItems.Ekwipunek.size():
-					if GlobalItems.Ekwipunek[item] == null:
+					if GlobalItems.Ekwipunek[item] == -1:
 						GlobalItems.Ekwipunek[item] = ItemId
 						self.visible = false	
 						var collision_shape_3d: CollisionShape3D = $StaticBody3D/CollisionShape3D
--- Scenes/Menus/equipment_bar/item_button.gd
+++ Scenes/Menus/equipment_bar/item_button.gd
@@ -1,6 +1,6 @@
 extends Button
 @export var button_number : int
-
+signal wybrano_item
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -12,6 +12,21 @@ func _process(delta: float) -> void:
 
 
 func _on_pressed() -> void:
-	print(button_number)
-	print(GlobalItems.Ekwipunek[button_number])
+	
+	GlobalSignals.Item_used.emit()
+	if(GlobalInput.Active_Item== -1):
+		GlobalInput.Active_Item = GlobalItems.Ekwipunek[button_number]
+		wybrano_item.emit()
+	else:
+		var globalinput_position = GlobalItems.Ekwipunek.find(GlobalInput.Active_Item)
+		if GlobalItems.Crafting.has(Vector2(GlobalItems.Ekwipunek[button_number],GlobalInput.Active_Item)):
+			var crafted = GlobalItems.Crafting[Vector2(GlobalItems.Ekwipunek[button_number],GlobalInput.Active_Item)]
+			GlobalItems.Ekwipunek[button_number] = -1
+			GlobalItems.Ekwipunek[globalinput_position] = -1
+			if(globalinput_position<button_number):
+				GlobalItems.Ekwipunek[globalinput_position] = crafted
+			elif (globalinput_position!=button_number):
+				GlobalItems.Ekwipunek[button_number] = crafted
+			else:
+				GlobalItems.Ekwipunek[button_number] = crafted
 	pass # Replace with function body.
