84f70d046ba88a71301fa0e171ded5b28f8b35bd
Oliwier Matus <matusoliwier@gmail.com>
2025-03-29 23:23:25 +0100

    fixed errors with click to move, added equipment icon after on click

--- Scenes/Menus/equipment_bar/item_button.gd
+++ Scenes/Menus/equipment_bar/item_button.gd
@@ -1,6 +1,7 @@
 extends Button
 @export var button_number : int
-signal wybrano_item
+#sygnał ogłaszający że nowy item został wybrany
+signal wybrano_item(position)
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -10,15 +11,16 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	pass
 
-
+#po kliknięciu na item emituje sygnał Item_used
 func _on_pressed() -> void:
-	
-	GlobalSignals.Item_used.emit()
-	if(GlobalInput.Active_Item== -1):
+	#jeżeli item pusty to zamienia aktywny item na pusty oraz emituje sygnał wybrano item
+	if(GlobalInput.Active_Item == -1):
 		GlobalInput.Active_Item = GlobalItems.Ekwipunek[button_number]
-		wybrano_item.emit()
+		wybrano_item.emit(button_number)
 	else:
+		# jeżeli item nie jest pusty to sprawdzamy jaki item jest w polu wcześniej aktywowanym który klikneliśmy
 		var globalinput_position = GlobalItems.Ekwipunek.find(GlobalInput.Active_Item)
+		# Jeżeli istnieje crafting reciepie z wybranego i obecnego itemu to go tworzy i umieszcza w polu wcześniejszym a pole późniejsze staje się puste
 		if GlobalItems.Crafting.has(Vector2(GlobalItems.Ekwipunek[button_number],GlobalInput.Active_Item)):
 			var crafted = GlobalItems.Crafting[Vector2(GlobalItems.Ekwipunek[button_number],GlobalInput.Active_Item)]
 			GlobalItems.Ekwipunek[button_number] = -1
@@ -29,4 +31,7 @@ func _on_pressed() -> void:
 				GlobalItems.Ekwipunek[button_number] = crafted
 			else:
 				GlobalItems.Ekwipunek[button_number] = crafted
+			GlobalSignals.Item_used.emit()
+		else:
+			wybrano_item.emit(button_number)
 	pass # Replace with function body.
