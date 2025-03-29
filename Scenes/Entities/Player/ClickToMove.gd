22f70b0c327a7608fcb5b3048a9ceff4a3038db6
Oliwier Matus <matusoliwier@gmail.com>
2025-03-29 10:10:13 +0100

    chodzenie nie działa

--- Scenes/Entities/Player/ClickToMove.gd
+++ Scenes/Entities/Player/ClickToMove.gd
@@ -2,8 +2,8 @@ extends CharacterBody3D
 
 
 	
-@export var main_game_mesh: MeshInstance3D
-@export var tutorial_mesh: MeshInstance3D
+@onready var main_game_mesh: MeshInstance3D = $MainGameMesh
+@onready var tutorial_mesh: MeshInstance3D = $TutorialMesh
 @onready var navigationAgent : NavigationAgent3D = $NavigationAgent3D
 
 var Speed = 5
@@ -12,6 +12,7 @@ var tutorial = true #Okreslamy czy tutorial trwa czy sie skonczyl
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	update_appearance()
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -31,17 +32,23 @@ func moveToPoint(_delta, speed):
 func faceDirection(direction):
 	look_at(Vector3(direction.x, global_position.y, direction.z), Vector3.UP)
 
+#sprawdzanie czy nie zostało coś wciśnięte
 func _input(_event):
 	if Input.is_action_just_pressed("LeftMouse"):
 		var Ekwipunek : Control = get_parent().get_node("Equipment/ObramowanieEkwipunku")
-		if not Ekwipunek.get_global_rect().has_point(get_viewport().get_mouse_position()):
+		#jeżeli wciśnięto lewy przycisk oraz nie został naciśnięty ekwipunek:
+		if (not Ekwipunek.get_global_rect().has_point(get_viewport().get_mouse_position()) or not Ekwipunek.get_parent().is_visible()):
+			#camera = pierwsza kamera w scenie
 			var camera = get_tree().get_nodes_in_group("Cameras")[0]
+			# sprawdzenie pozycji myszki na ekranie i wystrzelenie rayu o długości 100 w stronę myszki
 			var mousePos = get_viewport().get_mouse_position()
 			var rayLength = 100
 			var from = camera.project_ray_origin(mousePos)
 			var to = from + camera.project_ray_normal(mousePos) * rayLength
 			var space = get_world_3d().direct_space_state
 			var rayQuery = PhysicsRayQueryParameters3D.new()
+			#jeżeli coś zostanie trafione to tworzony jest punkt na nav meshu do którego porusza się gracz, Dla pewności że gracz nie spróbuje sie poruszyć "w gracza"
+			#zmieniono collision mask rayu aby nie interaktował z graczem
 			rayQuery.collision_mask = 4
 			rayQuery.from = from
 			rayQuery.to = to
