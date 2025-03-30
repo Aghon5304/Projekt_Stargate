55bccd5e1e615de88facd53e6d91fae591c2959f
CKLG-afk <kubkarwalski@gmail.com>
2025-03-30 17:16:23 +0200

    Poprawiono drzwi, przechodzi miedzy poziomami, poprawa warstw, oraz ulozenie map i lokacji zgodnie z wzorcem projektowym

--- Scenes/Entities/Door/door.gd
+++ Scenes/Entities/Door/door.gd
@@ -1,19 +1,26 @@
 extends Area3D
 
 @export var next_scene: String
+@export var door_type: String = "room"  # "room" lub "bathroom"
+@onready var mesh_instance: MeshInstance3D = $meshInstance3D
 
 var player_near = false
 
-func _on_body_entered(body: Node3D) -> void:
-	if body.is_in_group("player"):
-		player_near = true
-
-
-func _on_body_exited(body: Node3D) -> void:
-	if body.is_in_group("player"):
-		player_near = false
-
-
-func _on_input_event(camera: Node, event: InputEvent, event_position: Vector3, normal: Vector3, shape_idx: int) -> void:
-	if event.is_action_pressed("LeftMouse") and player_near:
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("action") and player_near:
 		get_tree().change_scene_to_file(next_scene)
+
+func _ready():
+	if door_type == "room":
+		mesh_instance.mesh = preload("res://Assets/mesh/doors/MainDoorMesh.tres")
+	elif door_type == "bathroom":
+		mesh_instance.mesh = preload("res://Assets/mesh/doors/bathroomDoorMesh.tres")
+	pass
+
+func _on_body_entered(body: Node3D) -> void:
+	if body.is_in_group("Player"):
+		player_near = true
+	
+func _on_body_exited(body: Node3D) -> void:
+	if body.is_in_group("Player"):
+		player_near = false
