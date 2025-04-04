074f37772b3e7cc9e4bad743da2ae4e1dac76916
Oliwier Matus <matusoliwier@gmail.com>
2025-04-04 16:46:25 +0200

    Final fixes

--- Scenes/Menus/Zagadki/lodówka.gd
+++ Scenes/Menus/Zagadki/lodówka.gd
@@ -3,17 +3,19 @@ var ItemId = GlobalItems.ItemTypes.KLUCZ_W_LODZIE
 @onready var transition_screen: AnimationPlayer = $"../../Transition_screen/AnimationPlayer"
 @onready var texture_rect: TextureRect = $"../TextureRect"
 var lodowka_bez_lodu :Texture2D = preload("res://Assets/Images/Fridge_Inside_Empty.png")
+var kliknięto := false
 func _ready() -> void:
 	if GlobalZagadkiState.klucz_w_lodówce == true:
 		texture_rect.texture = lodowka_bez_lodu
 
 func _on_pressed() -> void:
 	for item in GlobalItems.Ekwipunek.size():
-		if self.visible == true:
+		if self.visible == true and !kliknięto:
 			#jeżeli pole jest puste (-1) to zamienia je na ItemId (enum przedmiotu)
 			if GlobalItems.Ekwipunek[item] == -1:
 				GlobalItems.Ekwipunek[item] = ItemId
 				#przedmiot staje się niewidzalny oraz colishion shape się wyłącza aby symulować podniesienie
+				kliknięto = true
 				transition_screen.play("fade_to_black")
 				await transition_screen.animation_changed
 				texture_rect.texture = lodowka_bez_lodu
