38565bf2f906ad7d52e716284c07fac7e5de8f37
CKLG-afk <kubkarwalski@gmail.com>
2025-04-06 19:54:39 +0200

    Dodanie przejscia do glownego menu i poprawka do napisow koncowych

--- Scenes/Locations/end_location/end_credits.gd
+++ Scenes/Locations/end_location/end_credits.gd
@@ -7,6 +7,7 @@ func _ready() -> void:
 	var tween = get_tree().create_tween()
 	tween.tween_property(credits_label, "position:y", 500, 10).set_trans(Tween.TRANS_LINEAR)	
 	await tween.finished
+	get_tree().change_scene_to_file("res://Scenes/Menus/main_menu/main_menu.tscn")
 	#get_tree().quit()
 
 
