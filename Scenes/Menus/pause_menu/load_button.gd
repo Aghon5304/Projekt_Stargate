4254da8bbb293da78ad23aaae69198a98155861a
Oliwier Matus <95776763+Aghon5304@users.noreply.github.com>
2025-05-22 22:06:26 +0200

    save and load (#22)
    * fixed equipment
    
    * save and load game state
    
    * added save and load buttons to the pause menu
    
    * added continue functionality

--- Scenes/Menus/pause_menu/load_button.gd
+++ Scenes/Menus/pause_menu/load_button.gd
@@ -4,6 +4,5 @@ extends Button
 func _on_pressed() -> void:
 	GlobalSaveManager.Load_game_state()
 	var new_scene :PackedScene= GlobalGameProgress.Locations_dict.find_key(GlobalGameProgress.Current_Location).instantiate()
-	if new_scene != null:
-		get_tree().change_scene_to_packed(new_scene)
+	get_tree().change_scene_to_packed(new_scene)
 	pass 
