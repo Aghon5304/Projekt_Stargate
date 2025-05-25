faa776682f07b4e91c2c658ef1bc622cba9cdd59
seba1288 <seba128818@gmail.com>
2025-05-25 22:04:17 +0200

    changed main menu to start in motel

--- Scenes/Menus/main_menu/new_game_button.gd
+++ Scenes/Menus/main_menu/new_game_button.gd
@@ -1,7 +1,7 @@
 extends Button
 
 @onready var to_prologue = "res://Scenes/Menus/prologue/prologue.tscn"
-
+@onready var motel = "res://Scenes/Locations/Motel_outdors/Motel_parking.tscn"
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -12,4 +12,4 @@ func _process(delta: float) -> void:
 	pass
 
 func _on_pressed_new_game_button() -> void:
-	get_tree().change_scene_to_file(to_prologue) # Replace with function body.
+	get_tree().change_scene_to_file(motel) # Replace with function body.
