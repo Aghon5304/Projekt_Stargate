cea09e7a4b468345f2479ececdb675c92ab98de9
seba1288 <seba128818@gmail.com>
2025-05-31 11:09:01 +0200

    fixed game paused after changing scene

--- Scenes/Menus/pause_menu/pause.gd
+++ Scenes/Menus/pause_menu/pause.gd
@@ -5,9 +5,9 @@ extends Control
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	MainMenu.is_main_menu = false
+	GlobalSignals.is_not_paused = true
 	pauseMenu()
 	
-	pause_menu.position = Vector2(5000,5000)
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	
