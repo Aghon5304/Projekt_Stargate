da527546edd47d51f6562ae885d7d3d0fadf9c16
Oliwier Matus <95776763+Aghon5304@users.noreply.github.com>
2025-07-06 20:56:13 +0200

    fixed giving fishcola (#33)

--- Scenes/Locations/Motel_outdors/npc_renting_nr__2.gd
+++ Scenes/Locations/Motel_outdors/npc_renting_nr__2.gd
@@ -1,4 +1,4 @@
-extends StaticBody3D
+extends Node3D
 
 func _ready() -> void:
 	Dialogic.signal_event.connect(_on_dialogic_signal)
@@ -10,5 +10,7 @@ func _on_interactable_object_signal_item_activated() -> void:
 func _on_dialogic_signal(argument: String):
 	if argument == "dano_fishcole":
 		GlobalGameProgress.Progress["Motel_dano_fishcole"] = true
+		GlobalItems.Ekwipunek[GlobalItems.Ekwipunek.find(GlobalItems.ItemTypes.FISHCOLA)] = -1
+		GlobalSignals.Item_used
 	if argument == "Pierwsza_rozmowa":
 		GlobalGameProgress.Progress["Motel_pierwsza_pani_od_fishcoli"] = true
