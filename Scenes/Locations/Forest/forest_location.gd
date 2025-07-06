c1e4f1e74a8183282edc4a6458ddf44c8fb1f839
seba1288 <seba128818@gmail.com>
2025-07-06 19:04:59 +0200

    added hal of puzzle

--- Scenes/Locations/Forest/forest_location.gd
+++ Scenes/Locations/Forest/forest_location.gd
@@ -1,5 +1,4 @@
 extends  Node3D
 
-func _ready() -> void:
-	GlobalItems.Ekwipunek.insert(0,7)
+
 	pass
--- Scenes/Locations/Forest/npc.gd
+++ Scenes/Locations/Forest/npc.gd
@@ -1,20 +1,21 @@
 extends Node3D
 
-
+@onready var fence  = $"../Fence Export"
+var count = 0
 
 func _on_interactable_object_signal_item_activated() -> void:
-	Dialogic.start("guard_timeline")
-	pass
-
-
-func _on_interactable_object_signal_2_item_activated() -> void:
 	Dialogic.VAR.have_pass = 1
 	GlobalItems.Ekwipunek.assign([-1,-1,-1,-1,-1,-1,-1,-1])
 	Dialogic.start("guard_timeline")
+	if fence != null:
+		fence.queue_free()
+
+func _ready() -> void:
+	GlobalItems.Ekwipunek.insert(0,7)
 	
-
-
-func _on_interactable_object_signal_2_visibility_changed() -> void:
-	Dialogic.VAR.have_pass = 1
-	GlobalItems.Ekwipunek.assign([-1,-1,-1,-1,-1,-1,-1,-1])
-	Dialogic.start("guard_timeline")
+func _process(delta: float) -> void:
+	if count == 0:
+		if 11 in GlobalItems.Ekwipunek:
+			Dialogic.start("after_walkie-talkie_puzzle")
+			count += 1
+			$".".hide()
